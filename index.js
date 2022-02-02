adc0926004ce4cc1ed7dfff48eca5e4fdf58511a
stupidCoder-arunsks <arunsks57@gmail.com>
2022-02-02 14:38:36 +0530

    learned dom using getelementbyid

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // console.log(document);
-console.log(document.all);
+// console.log(document.all);
 // console.log(document.doctype)
-console.log(document.all[9]);
-document.all[9].innerHTML = '<h6>js pro</h6>';
+// console.log(document.all[9]);
+// document.all[9].innerHTML = '<h6>js pro</h6>';
+
+//Getting element by id
+
+// var mainHeader = document.getElementById('main-header');
+// console.log(mainHeader);
+// mainHeader.textContent = 'js for begin';
+// mainHeader.innerText = 'js to begin';
+// console.log(mainHeader.textContent);
+// console.log(mainHeader.innerText);
+// mainHeader.innerHTML = '<h2> < js for beginners > </h2>';
+// console.log(mainHeader);
+// mainHeader.style.borderBottom = 'solid 3px #000';
+
+var item1 = document.getElementById('item-header');
+item1.style.color = 'green';
+item1.style.fontSize = 'bold';
+
